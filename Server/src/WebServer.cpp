ff2fddb46f7a9ee1e70158e78c0622230421df41
bacox <b.a.cox@student.tudelft.nl>
2019-11-11 16:26:10 +0100

    Get transaction via the web ui

--- Server/src/WebServer.cpp
+++ Server/src/WebServer.cpp
@@ -4,7 +4,7 @@
 
 #include "WebServer.h"
 #include "../../Util/include/TransactionTestGenerator.h"
-
+#include "../../Util/include/Log.h"
 
 WebServer::WebServer(int port) : port(port), server(port){
     configure();
@@ -18,10 +18,12 @@ void WebServer::configure() {
 }
 
 void WebServer::start() {
+    EN_INFO("Starting server");
     server.start();
 }
 
 void WebServer::stop() {
+    EN_INFO("Stopping server");
     server.stop();
 }
 
@@ -37,4 +39,9 @@ void WebServer::generateFakeData() {
         Transaction *t = gen.generate();
         TransactionApi::create((*t));
     }
+}
+
+void WebServer::reload() {
+    stop();
+    start();
 }
